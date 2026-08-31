28bedbe181afae921ae0072115254f88d988a579
t-minami <minamiwave@gmail.com>
2026-08-31 10:42:25 +0900

    add comment02

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,7 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 
-// My tests
+// My tests 02
 
 describe('MainContent', () => {
   it('should render a button', () => {
